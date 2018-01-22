dcc6ce6cd2e502592104a3cf3d1bea8b28b93f0e
bo <389616868@qq.com>
2018-01-23 00:43:57 +0800

    2018-1-2

--- src/main/java/com/fyb/servlet/UsersFindByIdServlet.java
+++ src/main/java/com/fyb/servlet/UsersFindByIdServlet.java
@@ -2,6 +2,7 @@ package com.fyb.servlet;
 
 import com.fyb.dao.UsersDAO;
 import com.fyb.entity.Users;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,16 +16,23 @@ import java.io.IOException;
  */
 @WebServlet(name = "UsersFindByIdServlet",urlPatterns = "/user_detail")
 public class UsersFindByIdServlet extends HttpServlet {
+
+    /*
+   创建对应的日志记录对象
+   通过不同的级别进行日志的记录【DEBUG\WARN\INFO]
+    */
+    private Logger log = Logger.getLogger(UsersFindByIdServlet.class);
+
     private UsersDAO usersDAO = new UsersDAO();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String id=request.getParameter("id");
-
+        log.info("获取到查询参数id--》" + id);
         Users user=usersDAO.findById(Integer.parseInt(id));
 
         request.setAttribute("user",user);
-
+        log.info("查询数完成，查询到的数据：" + user);
         request.getRequestDispatcher("user_detail.jsp").forward(request,response);
 
     }
